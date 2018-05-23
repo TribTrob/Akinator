fa99a9e0633b13d446ecaaf86cd9eb940af88283
TribTrob <trib073@gmail.com>
2018-05-23 14:46:03 +0200

    List Handling added

--- akinator.py
+++ akinator.py
@@ -1,22 +1,19 @@
 from get_list_from_file import get_list_from_file
 import random
 
+
 def list_handling(people, attribute):
+    new_people = []
     for person in people:
-        if attribute not in person:
-            print(person)
-            people.remove(person)
-
-
-
-
-
-
-
+        if attribute in person:
+            new_people.append(person)
+    people = new_people
+    return people
 
 
 def main():
-    list_handling(get_list_from_file("emberek.txt"), "woman")
+    list_handling(get_list_from_file("emberek.txt"), "man")
+
 
 if __name__ == '__main__':
     main()
